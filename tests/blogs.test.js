e91090705ba0f709def7031bf32a267a60acb46f
vedhagiri <giri@brewengage.com>
2020-07-13 10:33:56 +0530

    invalid input usecase testing.

---
+++ tests/blogs.test.js
@@ -0,0 +1,39 @@
+const Page = require('./helpers/page');
+
+let page;
+beforeEach(async () => {
+    page = await Page.build();
+    await page.goto('localhost:3000');
+});
+
+afterEach( async () => {
+    await page.close();
+});
+
+
+
+describe('When logged in', async () => {
+    beforeEach( async () => {
+        await page.login();
+        await page.click('a.btn-floating');
+    });
+    test('can see blog create form', async () => {
+        const text = await page.getContentsOf('form label');
+        expect(text).toEqual('Blog Title');
+    });
+
+
+    describe('And using invalid inputs', async () => {
+        beforeEach( async () => {
+            await page.click('form button');
+        });
+        
+        test('the form shows an error message', async () => {
+            const titleError = await page.getContentsOf('.title .red-text');
+            const contentError = await page.getContentsOf('.content .red-text');
+            expect(titleError).toEqual('You must provide a value');
+            expect(contentError).toEqual('You must provide a value');
+        });
+
+    });
+});
--- tests/header.test.js
+++ tests/header.test.js
@@ -1,22 +1,17 @@
-const puppeteer = require('puppeteer');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
+const Page = require('./helpers/page');
 
-var browser, page;
+var page;
 beforeEach(async () => {
-    browser = await puppeteer.launch({
-        headless: false
-    });
-    page = await browser.newPage();
+    page = await Page.build();
     await page.goto('http://localhost:3000/');
 });
 
 afterEach(async () => {
-    await browser.close();
+    await page.close();
 });
-// describe('an essay on the best flavor', () => {
+
 test('The header has the correct text', async () => {
-    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    const text = await page.getContentsOf('a.brand-logo');
     expect(text).toEqual('Blogster');
 });
 
@@ -25,26 +20,9 @@ test('Clicking login start oAuth flow', async ()=> {
     const url = await page.url();
     expect(url).toMatch(/accounts\.google\.com/);
 });
-// });
+
 test('When signIn, show the logout button', async () =>{
-    // const id = '5ef578a5995e666a495183eb';
-    const user  = await userFactory();
-    console.log('User Records:', user);
-    const { session, sig } = sessionFactory(user);
-    console.log(session, sig);
-    // eyJwYXNzcG9ydCI6eyJ1c2VyIjoiNWVmNTc4YTU5OTVlNjY2YTQ5NTE4M2ViIn19
-    // 4_aqp6hV-e4_31yYeV6gshLdjbM
-    await page.setCookie({ name: 'session', value: session});
-    await page.setCookie({ name: 'session.sig', value: sig});
-    await page.goto('http://localhost:3000/');
-
-    await page.waitFor('a[href="/auth/logout"]');
-    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
-
-    console.log(text);
+    await page.login();
+    const text = await page.getContentsOf('a[href="/auth/logout"]');
     expect(text).toEqual('Logout');
-
 });
-
-
-// {"_id":{"$oid":"5ef578a5995e666a495183eb"},"googleId":"112328781197817998111","displayName":"Vedhagiri Prakasam","__v":{"$numberInt":"0"}}
