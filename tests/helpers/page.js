29e66f57f0de6d58b4915ab2fda79497e060dc57
vedhagiri <giri@brewengage.com>
2020-07-14 20:09:08 +0530

    ci changes.

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -5,7 +5,11 @@ const sessionFactory = require('../factories/sessionFactory');
 class CustomPage {
 
     static async build() {
-        const browser = await puppeteer.launch({ headless: false});
+        const browser = await puppeteer.launch(
+            { 
+                headless: true,
+                args: ['--no-sandbox']
+            });
 
         const page = await browser.newPage();
         const customPage = new CustomPage(page);
